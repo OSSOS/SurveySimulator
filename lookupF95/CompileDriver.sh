e11b432e62a213d4c6116325b4f73df11e5e6121
Jean-Marc Petit <jean-marc.petit@normalesup.org>
2019-03-31 18:38:04 +0200

    More little fixings. 2019-03-31T16:38 UT.

--- lookupF95/CompileDriver.sh
+++ lookupF95/CompileDriver.sh
@@ -2,6 +2,8 @@
 
 \rm -f SurveySimulator
 cd ../srcF95
+\rm -f gimeobjut.f95
+ln -s ../lookupF95/ReadModelFromFile.f95 gimeobjut.f95
 make Driver
 cd ../lookupF95
 ln -s ../srcF95/Driver SurveySimulator
