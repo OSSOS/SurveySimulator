743946c0cae75ccd361c64bda9596370cb96dfae
JJ Kavelaars <jjkavelaars@gmail.com>
2022-08-01 09:19:15 -0700

    Make the F77 and F95 test scripts and README files identical

--- F77/example/test.sh
+++ F77/example/test.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 cmd=$1
 src_dir=$(dirname ${1})
 cmd=$(basename ${1})
@@ -11,7 +11,14 @@ if [ -f SimulTrack.dat ]; then
     \rm SimulTrack.dat
 fi
 
-time ${src_dir}/Driver < ${cmd}.in > LOG
+${src_dir}/Driver < ${cmd}.in
+if [ $? != 0 ]; then
+  status=$?
+  echo "================================================================="
+  echo "=======================  Test FAILED!   ========================="
+  echo "================================================================="
+  exit ${status}
+fi
 
 a=`head -10 SimulDetect.dat | tail -1 | awk '{print $1}'`
 s=`head -10 SimulDetect.dat | tail -1 | awk '{print $15}'`
@@ -24,7 +31,7 @@ ndo=`tail -3 ${cmd}-check.dat | head -1 | awk '{printf "%10d", $6}'`
 ndc=`tail -2 ${cmd}-check.dat | head -1 | awk '{print $5}'`
 ntc=`tail -1 ${cmd}-check.dat | awk '{print $6}'`
 
-if [ $a != $ac ] || [ $s != $sc ]; then
+if [ $a != $ac -o $s != $sc ]; then
     echo "Error: first detection doesn't match!"
     exit 1
 fi
@@ -32,14 +39,16 @@ if [ $no != $ndo ]; then
     echo "Error: wrong number of objects!"
     exit 1
 fi
-if [ $nd -gt $(($ndc + 10)) ] || [ $nd -lt $(($ndc - 10)) ]; then
+if [ $nd -gt $(($ndc + 10)) -o $nd -lt $(($ndc - 10)) ]; then
     echo "Error: wrong number of detections!"
     exit 1
 fi
-if [ $nt -gt $(($ntc + 10)) ] || [ $nt -lt $(($ntc - 10)) ]; then
+if [ $nt -gt $(($ntc + 10)) -o $nt -lt $(($ntc - 10)) ]; then
     echo "Error: wrong number of tracking!"
     exit 1
 fi
-echo "Test successful!"
+echo "================================================================="
+echo "=====================  Test successful!  ========================"
+echo "================================================================="
 
 exit
